85cb1eed786b88e88c0cb1b8623ea6a870ce14c1
Shiv <shivasare101@gmail.com>
2023-03-03 18:16:43 +0530

    condition

---
+++ Switch _case/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	daysofeweek := 1
+	switch daysofeweek {
+	case 1:
+		{
+			fmt.Println("Monday")
+		}
+	case 2:
+		{
+			fmt.Println("tus")
+		}
+	case 3:
+		{
+			fmt.Println("Wed")
+		}
+	case 4:
+		{
+			fmt.Println("Thus")
+		}
+	case 5:
+		{
+			fmt.Println("Fri")
+		}
+	case 6:
+		{
+			fmt.Println("Set")
+		}
+	case 7:
+		{
+			fmt.Println("Sun")
+		}
+	default:
+		{
+			fmt.Println("ndfkljn ijrh")
+		}
+
+	}
+}
