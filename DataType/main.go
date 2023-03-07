41c835b66520ed8059cb34b4f2702775db26dd8b
Shiv <shivasare101@gmail.com>
2023-03-07 13:28:30 +0530

    data file add

---
+++ DataType/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a bool = true
+	var b int = 5
+	var c float32 = 123.78
+	var d string = "kjdfsbijr"
+
+	fmt.Println("Boolean:", a)
+	fmt.Println("integer :", b)
+	fmt.Println("float:", c)
+	fmt.Println("string:", d)
+}
